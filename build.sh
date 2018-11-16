8dc88c4f55e7c86a30e18bd2842e991d4325ed08
Stefan Friedl <friedl@abss.at>
2018-11-16 14:07:53 +0100

    feat: :sparkles: semantic-release

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 #
 # TBD: refactor: build scripts with ? nodemon | rollup | backpack?
 rm dist/* -rf
+mkdir dist
 
 npx babel ./src/index.js --out-file ./dist/index.js --source-maps inline --presets minify
 npx babel ./src/chalkExt.js --out-file ./dist/chalkExt.js --source-maps inline --presets minify
--- test/index.js
+++ test/index.js
@@ -3,4 +3,4 @@ import Logger from '../dist/index.js'
 Logger.success('logger running!')
 Logger.error('unexpected bahavior')
 Logger.log('{reset.magenta custom {bgBlackBright.yellow output style}}')
-Logger.debug('casual debug info')
+Logger.debug('standard debug info')
