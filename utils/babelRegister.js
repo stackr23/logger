58c90a1221f615d62823976b822f6f983cfe080a
DoubleU23 <dev@project23.org>
2018-11-16 23:37:25 +0100

    fix(changelog): added changelog title

--- utils/babelRegister.js
+++ utils/babelRegister.js
@@ -1,6 +1,8 @@
 'use strict'
 
 var path            = require('path'),
-    pathNodeModules = path.normalize(path.resolve(__dirname, '..', 'node_modules'))
+    pathNodeModules = path.normalize(
+    path.resolve(__dirname, '..', 'node_modules')
+)
 
 require('@babel/register')({ignore: [pathNodeModules]})
