988273d2d3ef5d60eb1d151ec9b92f400ee22af0
DoubleU23 <dev@project23.org>
2018-11-19 17:25:15 +0100

    refactor(♻️): build.sh -> src/* && --out-dir

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@
 rm dist/* -rf
 mkdir dist
 
-npx babel ./src/index.js --out-file ./dist/index.js --source-maps inline --presets minify
-npx babel ./src/chalkExt.js --out-file ./dist/chalkExt.js --source-maps inline --presets minify
+npx babel ./src/*.js --out-dir ./dist/ --source-maps inline --presets minify
